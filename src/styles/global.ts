719fc63729d23cd65371319dc0c47395966a76a7
mikasampaio <mikaasampaio6@gmail.com>
2024-04-28 08:56:40 -0300

    chore: configuração styled-components e tema

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { theme } from './theme';
+
 export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
@@ -10,5 +12,23 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         -webkit-font-smoothing: antialiased;
+        background-color: ${theme.colors.black};
+    }
+
+    body, input, button, select {
+        font: 1rem 'Lexend', sans-serif;
+    }
+
+    h1, h2, p, span, strong, button, label, input {
+        line-height: 100%;
+    }
+
+    #root {
+        max-width: 1280px;
+        margin: 0 auto;
+    }
+
+    button {
+        cursor: pointer;
     }
 `;
